5f1c7878d873afea231825d0b72d172527bf004b
Mohsen Fathipour <mohsenfathipour.com@gmail.com>
2024-01-14 13:31:09 +0330

    json response added.

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -1,4 +1,7 @@
 <?php
+
+use Illuminate\Http\JsonResponse;
+
 if (!function_exists('generateUniqueNumber')) {
     function generateUniqueNumber(int $length): int
     {
@@ -9,5 +12,27 @@ if (!function_exists('generateUniqueNumber')) {
         $uniqueNumber = implode('', array_slice($digits, 0, $length));
 
         return (int)$uniqueNumber;
+    }
+}
+
+if (!function_exists('api_response')) {
+
+    /**
+     * @param bool $success
+     * @param $data
+     * @param $response_code
+     * @param $massage
+     * @param $errors
+     * @return JsonResponse
+     *
+     */
+    function api_response(bool $success, $data = null, $response_code = 200, $massage = null, $errors = null): JsonResponse
+    {
+        return response()->json([
+            'success' => $success,
+            'message' => $massage,
+            'data' => $data,
+            'errors' => $errors,
+        ], $response_code);
     }
 }
--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Game\GameStartRequest;
 use App\Models\Game;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
 /**
  * @group Game
  */
@@ -18,34 +21,52 @@ class GameController extends Controller
         $game->length = $request->length ?? 3;
         $game->number = generateUniqueNumber($game->length);
         $game->save();
-        return $game->id ;
+        return response()->json([
+            'success' => true,
+            'data' => ['game_id' => $game->id],
+        ], Response::HTTP_OK);
     }
 
     /*
      * try new number
      */
-    public function try($game , $num)
+    public function try($game, $num)
     {
         $game = Game::find($game);
 
-        if(!$game)
-            return 'Game not found!';
+        if (!$game)
+            return response()->json([
+                'success' => false,
+                'message' => 'Game not found!',
+                'data' => null,
+            ], Response::HTTP_NOT_FOUND);
 
-        if($game->status_enum == 'done')
-            return 'Game is finished!';
+        if ($game->status_enum == 'done')
+            return response()->json([
+                'success' => false,
+                'message' => 'Game is finished!',
+                'data' => null,
+            ], Response::HTTP_REQUEST_TIMEOUT);
 
-        if($game->length != strlen($num))
-            return 'Number length should be ' . $game->length;
+        if ($game->length != strlen($num))
+            return response()->json([
+                'success' => false,
+                'message' => 'Number length should be ' . $game->length,
+                'data' => null,
+            ], Response::HTTP_NOT_ACCEPTABLE);
 
         $corrects = 0;
         $misplaces = 0;
 
-        $game_nums  = str_split($game->number);
-        $try_nums  = str_split($num);
+        $game_nums = str_split($game->number);
+        $try_nums = str_split($num);
 
-        if($try_nums != array_unique($try_nums) )
-            return 'Numbers can not be duplicated!';
-
+        if ($try_nums != array_unique($try_nums))
+            return response()->json([
+                'success' => false,
+                'message' => 'Numbers can not be duplicated!',
+                'data' => null,
+            ], Response::HTTP_NOT_ACCEPTABLE);
 
         # increase try;
         $game->try++;
@@ -53,24 +74,41 @@ class GameController extends Controller
 
         # corrects:
         foreach ($game_nums as $i => $game_num)
-            if($try_nums[$i] == $game_num)
+            if ($try_nums[$i] == $game_num)
                 $corrects++;
 
         # win? :
-        if($corrects == $game->length){
+        if ($corrects == $game->length) {
             $game->status_enum = 'done';
             $game->end_at = date('Y-m-d H:i:s');
             $game->save();
-            return 'You win! the number was ' . $game->number . ' try: ' . $game->try;
+
+            return response()->json([
+                'success' => true,
+                'message' => 'You win!',
+                'data' => [
+                    'number' => $game->number,
+                    'try'   => $game->try
+                ],
+            ], Response::HTTP_OK);
         }
 
         # misplaces
-        foreach ($try_nums as $i => $try_num){
-            if(in_array($try_num,$game_nums)
+        foreach ($try_nums as $i => $try_num) {
+            if (in_array($try_num, $game_nums)
                 && $game_nums[$i] != $try_num)
                 $misplaces++;
         }
 
-        return ['corrects' => $corrects,'misplaces' => $misplaces];
+        return response()->json([
+            'success' => false,
+            'message' => 'Wrong!',
+            'data' => [
+                'num' => $num,
+                'try'   => $game->try,
+                'corrects' => $corrects,
+                'misplaces' => $misplaces
+            ],
+        ], Response::HTTP_BAD_REQUEST);
     }
 }
